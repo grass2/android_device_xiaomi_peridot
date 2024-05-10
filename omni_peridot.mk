c1c2e86d066508063e337f38001f25b6365cf576
grass2 <cuongdieu@gmail.com>
2024-05-10 14:04:24 +0700

    Update twrp_peridot.mk

--- omni_peridot.mk
+++ omni_peridot.mk
@@ -10,15 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
 PRODUCT_DEVICE := peridot
-PRODUCT_NAME := omni_peridot
+PRODUCT_NAME := twrp_peridot
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 24069RA21C
+PRODUCT_MODEL := Redmi Turbo 3
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
