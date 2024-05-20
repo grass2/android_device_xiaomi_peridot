1de8b8cfce22837cfc5860faa55569476f6a6d7d
grass2 <cuongdieu@gmail.com>
2024-05-20 16:57:03 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_peridot-user
-add_lunch_combo twrp_peridot-userdebug
-add_lunch_combo twrp_peridot-eng
+# add_lunch_combo twrp_peridot-user
+# add_lunch_combo twrp_peridot-userdebug
+# add_lunch_combo twrp_peridot-eng
+
+export ALLOW_MISSING_DEPENDENCIES=true
